f7634d0def299512a3b5481eb05c9817620800e0
slymnhlm123 <142054022+slymnhlm123@users.noreply.github.com>
2025-02-07 18:40:22 +0300

    İkinci Sayfanın frame'i oluşturuldu

--- untitled/src/main/java/org/example/AçılanSayfa.java
+++ untitled/src/main/java/org/example/AçılanSayfa.java
@@ -1,4 +1,28 @@
 package org.example;
 
-public class AçılanSayfa {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AçılanSayfa implements ActionListener
+{
+    JFrame frame = new JFrame();
+
+    public AçılanSayfa()
+    {
+        frame.setSize(420,420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(1,2));
+
+
+
+
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
