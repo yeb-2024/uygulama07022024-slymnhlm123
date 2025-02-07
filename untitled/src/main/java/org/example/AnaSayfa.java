29525b3d8a1f4576b13bc2ff3b195359e6991c3e
slymnhlm123 <142054022+slymnhlm123@users.noreply.github.com>
2025-02-07 18:34:08 +0300

    Buton Oluşturuldu ve AçılanSayfa adlı dosya oluşturuldu

--- untitled/src/main/java/org/example/AnaSayfa.java
+++ untitled/src/main/java/org/example/AnaSayfa.java
@@ -9,20 +9,28 @@ public class AnaSayfa implements ActionListener
 {
     JFrame frame = new JFrame();
 
+    JButton button = new JButton();
+
 
     public AnaSayfa()
     {
-
+        button.setText("Profil Aç");
+        button.addActionListener(this);
+        button.setFocusable(false);
+        button.setBounds(100,160,200,40);
 
         frame.setSize(420,420);
         frame.setBackground(Color.GRAY);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
+        frame.add(button);
         frame.setVisible(true);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e)
+    {
+
 
     }
 }
